b2944a393a3f93104d8029c77041c37753f91bfc
nezhuka <ingasom63@gmail.com>
2023-09-11 15:16:28 +0100

    Added links on GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <br />
+      <p>Coded by 
+        <a href="https://www.shecodes.io/reviews">
+        Inha Somsikova</a> and 
+        <a href="https://github.com/nezhuka/weather-react-app">
+          open-sourced on GitHub </a>
+          and
+        <a href="https://warm-blancmange-8f4413.netlify.app/">
+          deployed on Netlify.</a>
+          </p>
+        
+      
+
     </div>
   );
 }
