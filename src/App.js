eea5d25f2103347f80871a76b853625029c2f17f
nezhuka <ingasom63@gmail.com>
2023-09-11 15:22:40 +0100

    Added gaps

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ export default function App() {
       <h1>Weather App</h1>
       <SearchEngine />
       <br />
-      <p>Coded by 
+      <p>Coded by  
         <a href="https://www.shecodes.io/reviews">
-        Inha Somsikova</a> and 
+        Inha Somsikova</a>  and 
+
         <a href="https://github.com/nezhuka/weather-react-app">
-          open-sourced on GitHub </a>
-          and
-        <a href="https://warm-blancmange-8f4413.netlify.app/">
+          open-sourced on GitHub </a> 
+
+          and 
+        <a href="https://warm-blancmange-8f4413.netlify.app/"> 
+        
           deployed on Netlify.</a>
           </p>
         
